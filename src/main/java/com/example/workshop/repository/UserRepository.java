d6d4e650d4a74d82a5e8c685c52b0b6b35653bcc
Bighetto <arthurbighetto36@gmail.com>
2021-07-28 14:33:45 -0300

    adicao respository e service do User

---
+++ src/main/java/com/example/workshop/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.workshop.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.workshop.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+}
