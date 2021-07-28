d81606ab12803812d95493c5afc816f0a5c47552
Bighetto <arthurbighetto36@gmail.com>
2021-07-28 17:37:08 -0300

    Delecao usuario com o Delete

--- src/main/java/com/example/workshop/resources/UserResource.java
+++ src/main/java/com/example/workshop/resources/UserResource.java
@@ -46,6 +46,12 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
+	@RequestMapping(value = "/{id}", method=RequestMethod.POST)
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 	
 
 }
