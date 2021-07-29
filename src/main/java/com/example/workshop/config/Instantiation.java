913bf596f100f5854e3296bb2fbdbe4e301dede6
Bighetto <arthurbighetto36@gmail.com>
2021-07-29 15:49:23 -0300

    referenciando os posts de um usuario

--- src/main/java/com/example/workshop/config/Instantiation.java
+++ src/main/java/com/example/workshop/config/Instantiation.java
@@ -44,5 +44,11 @@ public class Instantiation implements CommandLineRunner{
 		
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1));
+		alex.getPosts().addAll(Arrays.asList(post2));
+		
+		userRepository.save(maria);
+		userRepository.save(alex);
 	}
 }
